bf060eb63b264e8f893414c9ff9b26ba1cb71090
hellomyzn <hellomyzn@gmail.com>
2023-06-25 18:06:58 +0900

    update (video service): change strip

--- src/services/video_service.py
+++ src/services/video_service.py
@@ -22,12 +22,12 @@ class VideoService(object):
 
         # retreive specific data
         videos = []
-        ads = []
+        video_ads = []
         videos_unable = []
 
         for w in watch_history_json_data:
             if 'details' in w:
-                ads.append(w)
+                video_ads.append(w)
                 continue
 
             if not 'subtitles' in w:
@@ -39,7 +39,7 @@ class VideoService(object):
             url = w['titleUrl']
             # 2023-06-18T12:12:24.318Z -> 2023-06-18 12:12:24
             # 2023-06-12T21:33:12Z -> 2023-06-12 21:33:12
-            timestamp_str = w['time'].replace("T", " ").replace("Z", "").split('.')[0]
+            timestamp_str = w['time'].replace("T", " ").strip("Z").split('.')[0]
             timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
 
             videos.append([title, channel, url, timestamp])
@@ -48,8 +48,8 @@ class VideoService(object):
         videos.reverse()
 
         logger_pro.info(f'videos: {len(videos)}')
-        logger_pro.info(f'ads: {len(ads)}')
+        logger_pro.info(f'ads: {len(video_ads)}')
         logger_pro.info(f'unable: {len(videos_unable)}')
 
-        return videos, ads, videos_unable
+        return videos, video_ads, videos_unable
     
